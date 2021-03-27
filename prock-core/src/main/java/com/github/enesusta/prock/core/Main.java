966d37fac949c95a49771bcb32caec2586e3e0a4
enesusta <enesusta@email.com>
2021-03-27 19:15:40 +0300

    Multimodule maven infra setup done

--- prock-core/src/main/java/com/github/enesusta/prock/core/Main.java
+++ prock-core/src/main/java/com/github/enesusta/prock/core/Main.java
@@ -2,8 +2,26 @@ package com.github.enesusta.prock.core;
 
 import com.github.enesusta.prock.components.grids.Grid;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
     public static void main(String[] args) {
         Grid grid = Grid.newInstance(3, 5);
+
+        try {
+            JFrame frame = new JFrame();
+            frame.getContentPane().add(grid);
+
+            UIManager.setLookAndFeel(UIManager.getLookAndFeel());
+            SwingUtilities.updateComponentTreeUI(frame);
+
+
+            frame.show();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ prock-playground/src/main/java/com/github/prock/playground/Main.java
@@ -0,0 +1,8 @@
+package com.github.prock.playground;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
